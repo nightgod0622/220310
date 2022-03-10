60e42317d2a92087dbe99d37a8b0a05aa4b573ba
趙健傑 <100336478+nightgod0622@users.noreply.github.com>
2022-03-10 15:04:12 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
+basic.forever(function () {
+	
+})
